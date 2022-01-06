03e2700fc944842ef04c124a8502c9c0156fefd8
plotinusspascual <plotinusspascual@gmail.com>
2022-01-06 11:44:52 -0800

    added setInterval to moment function to display current time every second

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 
-var currentTime = moment();
-$('#current-time').text(moment().format("L LT"));
+//var currentTime = moment();
+// $('#current-time').text(moment().format("L LT"));
+
+var currentTime = function(){
+  document.getElementById("current-time").innerHTML =
+  moment().format("MM/D/YYYY, h:m:ss");
+  }
+
+setInterval(currentTime, 1000); 
