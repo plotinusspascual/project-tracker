b23f394ae9e907b31b6907a80d89c01aa454f674
Laura Gupta <95309002+lauragupta@users.noreply.github.com>
2022-01-06 15:12:20 -0800

    adding some of localstorage functions.

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ var currentTime = function(){
   }
 
 setInterval(currentTime, 1000); 
+
+
+
+function getProjects(){
+  var storedProjects = JSON.parse(localStorage.getItem("project"));
+  if (storedProjects === null) {
+    return [];
+  }
+  return storedProjects;
+}
+
+function postProject(event) {
+  event.preventDefault();
+  // get items by id for values pairs
+  var newProject = {
+    projectName: projectName,
+    projectType: projectType,
+    dueDate: dueDate,
+    hourlyRate: hourlyRate,
+  }
+
+
+  var storedProject = getProjects();
+  storedProjects.unshift(newProject);
+  localStorage.setItem("projects", JSON.stringify(storedProjects));
+  renderProjects();
+}
+
+//add event listener for submit button
+submitButton.addEventListener("submit", postProjects);
